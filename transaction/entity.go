30e6f708451997821b83c56e846ea3f0264cca15
Agung Setiawan <agungsetiawan.jg@ruangguru.com>
2020-10-10 15:14:06 +0700

    transaction preparation

---
+++ transaction/entity.go
@@ -0,0 +1,14 @@
+package transaction
+
+import "time"
+
+type Transaction struct {
+	ID         int
+	CampaignID int
+	UserID     int
+	Amount     int
+	Status     string
+	Code       string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
