9e69d554ae71302498fb74c732e7e63fc58475e2
Agung Setiawan <agungsetiawan.jg@ruangguru.com>
2020-10-19 21:00:41 +0700

    midtrans integration

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"bwastartup/helper"
 	"bwastartup/transaction"
 	"bwastartup/user"
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strings"
@@ -15,6 +17,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+
+	midtrans "github.com/veritrans/go-midtrans"
 )
 
 func main() {
@@ -56,7 +60,85 @@ func main() {
 	api.GET("/campaigns/:id/transactions", authMiddleware(authService, userService), transactionHandler.GetCampaignTransactions)
 	api.GET("/transactions", authMiddleware(authService, userService), transactionHandler.GetUserTransactions)
 
+	api.POST("/transactions/notification", notification)
+
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = ""
+	midclient.ClientKey = ""
+	midclient.APIEnvType = midtrans.Sandbox
+
+	snapGateway := midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapReq := &midtrans.SnapReq{
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  "ORDER-33333",
+			GrossAmt: 13300000,
+		},
+		CustomerDetail: &midtrans.CustDetail{
+			FName: "Maudy Ayunda",
+			Email: "maudy@hatimu.com",
+		},
+	}
+
+	fmt.Println("Get token")
+	fmt.Println("Get token")
+	fmt.Println("Get token")
+	fmt.Println("Get token")
+	fmt.Println("Get token")
+	snapTokenResponse, err := snapGateway.GetToken(snapReq)
+	if err != nil {
+		fmt.Println("error :")
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(snapTokenResponse.Token)
+	fmt.Println(snapTokenResponse.RedirectURL)
+
 	router.Run()
+}
+
+func notification(c *gin.Context) {
+	var reqPayload = &midtrans.ChargeReqWithMap{}
+	err := json.NewDecoder(c.Request.Body).Decode(reqPayload)
+	if err != nil {
+		response := make(map[string]interface{})
+		response["status_code"] = 400
+		response["status_message"] = "please fill request payload, refer to https://api-docs.midtrans.com/#receiving-notifications"
+
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	encode, _ := json.Marshal(reqPayload)
+	resArray := make(map[string]string)
+	err = json.Unmarshal(encode, &resArray)
+
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = ""
+	midclient.ClientKey = ""
+	midclient.APIEnvType = midtrans.Sandbox
+
+	coreGateway := midtrans.CoreGateway{
+		Client: midclient,
+	}
+
+	chargeResp, _ := coreGateway.StatusWithMap(resArray["order_id"])
+	result, err := json.Marshal(chargeResp)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, nil)
+		return
+	}
+
+	fmt.Println("RESULT")
+	fmt.Println("RESULT")
+	fmt.Println("RESULT")
+	fmt.Println(string(result))
+	fmt.Println("RESULT")
+	fmt.Println("RESULT")
+
+	c.JSON(http.StatusOK, result)
 }
 
 func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
